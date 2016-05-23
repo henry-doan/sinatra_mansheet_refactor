163f2a5876c582c0344ca54c86d7366dd06b03e8
Henry Doan <henrydoan96@gmail.com>
2016-05-23 09:06:36 -0600

    Basic

---
+++ cheatsheet.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'pry'
+
+get '/' do
+	erb :main
+end
+
+get '/cl' do
+	erb :cl
+end
+
+get '/search' do
+	erb :search
+end
+get '/results' do
+	erb :results
+end
+
+get '/mv' do
+	erb :result
+	`man mv`
+end
+
+get '/cp' do
+	`man cp`
+end
+
+get '/mkdir' do
+	`man mkdir`
+end
+
+get '/ls' do
+	`man ls`
+end
+
+get '/rm' do
+	`man rm`
+end
